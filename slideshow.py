ec2c922d275de258b9c765a18e21da3a1353324e
Jordan White <jordanrw@vt.edu>
2019-03-15 19:37:10 -0700

    Add iterate through all images with keys

--- slideshow.py
+++ slideshow.py
@@ -6,8 +6,12 @@ from tkinter import Tk, Label
 from PIL import ImageTk, Image
 
 class Slideshow:
+    _DIRECTORY = 'hike4/'
+    _EXTENSION = '.jpg'
+    # FILE_PATH = ''
     FILE_PATH = 'hike4/2.jpg'
-    EXTENSION = 'jpg'
+    COUNT = 0
+    _LIMIT = 241
 
     def __init__(self, win):
         self.window = win
@@ -15,6 +19,7 @@ class Slideshow:
         self.window.geometry("1280x720")
         self.window.configure(background='black')
 
+        self.moveFilePointer('+')
         self.img = ImageTk.PhotoImage(Image.open(self.FILE_PATH, 'r'))
         self.picture_label = Label(master=self.window, image=self.img)
         self.picture_label.pack(side="bottom", fill="both", expand="yes")
@@ -23,24 +28,38 @@ class Slideshow:
         self.window.bind('<Right>', self.rightKey)
 
     def showImage(self):
-        self.img = ImageTk.PhotoImage(Image.open('hike4/4.jpg', 'r'))
+        self.img = ImageTk.PhotoImage(Image.open(self.FILE_PATH, 'r'))
         self.picture_label.configure(image=self.img)
 
-    def moveFilePointer(self):
-        print('howdy')
+    def moveFilePointer(self, command):
+        if command == '+':
+            self.COUNT += 1
+            if self.COUNT > self._LIMIT:
+                self.COUNT = 1
+        elif command == '-':
+            self.COUNT -= 1
+            if self.COUNT < 1:
+                self.COUNT = self._LIMIT
+        else:
+            raise Exception('command should be either '+' or '-'')
+
+        self.FILE_PATH = self._DIRECTORY + str(self.COUNT) + self._EXTENSION
+        sys.stdout.flush()
+        print(self.FILE_PATH)
+        sys.stdout.flush()
 
     def rightKey(self, event):
         sys.stdout.flush()
         print("Increment the count")
         sys.stdout.flush()
-        self.moveFilePointer()
+        self.moveFilePointer('+')
         self.showImage()
 
     def leftKey(self, event):
         sys.stdout.flush()
         print("Decrement the count")
         sys.stdout.flush()
-        self.moveFilePointer()
+        self.moveFilePointer('-')
         self.showImage()
     
 # This creates the root window
