282e0499a9e747bcc21b2e278d15fb2f2a9d5c88
jordanrw <jordan_white@sfu.ca>
2019-03-27 20:04:47 -0700

    Add a bunch of queue and rotary encoder stuff that isn't working yet

--- rotary-encoder.py
+++ rotary-encoder.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/python
-
 from gpiozero import Button
-from RPi import GPIO
-import Queue
+#from RPi import GPIO
+# import Queue
+from queue import *
 
-eventq = Queue.Queue()
+eventq = Queue()
 
-pin_a = Button(17)                      # Rotary encoder pin A connected to GPIO2
-pin_b = Button(18)                      # Rotary encoder pin B connected to GPIO3
+pin_a = Button(17)                      # Rotary encoder pin A connected to GPI17
+pin_b = Button(18)                      # Rotary encoder pin B connected to GPI18
 
-def pin_a_rising():                    # Pin A event handler
-    if pin_b.is_pressed: eventq.put(-1)# pin A rising while A is active is a clockwise turn
 
-def pin_b_rising():                    # Pin B event handler
-    if pin_a.is_pressed: eventq.put(1) # pin B rising while A is active is a clockwise turn
+# Pin A event handler
+def pin_a_rising():
+    if pin_b.is_pressed:
+        print('-1')
+        # eventq.put(-1) # pin A rising while B is active is a counter-clockwise turn
 
-pin_a.when_pressed = pin_a_rising      # Register the event handler for pin A
-pin_b.when_pressed = pin_b_rising      # Register the event handler for pin B
+
+# Pin B event handler
+def pin_b_rising():
+    if pin_a.is_pressed:
+        print('1')
+        # eventq.put(1) # pin B rising while A is active is a clockwise turn
+
+
+# Register the event handler for pin A
+pin_a.when_pressed = pin_a_rising
+# Register the event handler for pin B
+pin_b.when_pressed = pin_b_rising
 
 while True:
     message = eventq.get()
--- slideshow.py
+++ slideshow.py
@@ -4,8 +4,10 @@
 import sys
 from PIL import ImageTk, Image
 from RPi import GPIO
+from gpiozero import Button
 from time import sleep
 from tkinter import Tk, Label
+import queue
 
 # Setup for GPIO
 clk = 17
@@ -15,17 +17,22 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(cnt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+
 # Slideshow class which is the main class that runs and is listening for events
 class Slideshow:
     _DIRECTORY = 'hike4/'
+    # _DIRECTORY = 'hike4-tiny/'
     _EXTENSION = '.jpg'
     # FILE_PATH = ''
-    FILE_PATH = 'hike4/2.jpg'
+    FILE_PATH = 'hike4-tiny/2.jpg'
     COUNT = 0
     _LIMIT = 241
     
     ROTARY_COUNTER = 1
     clkLastState = GPIO.input(clk)
+
+    # pin_a = Button(17)  # Rotary encoder pin A connected to GPI17
+    # pin_b = Button(18)  # Rotary encoder pin B connected to GPI18
 
     def __init__(self, win):
         self.window = win
@@ -42,20 +49,49 @@ class Slideshow:
         self.window.bind('<Right>', self.rightKey)
         
         self.window.bind(GPIO.add_event_detect(clk, GPIO.BOTH, callback=self.detectedRotaryChange))
+
+        # Register the event handler for pin A
+        # self.pin_a.when_pressed = self.pin_a_rising
+        # Register the event handler for pin B
+        # self.pin_b.when_pressed = self.pin_b_rising
+
+        # self.the_queue = queue.Queue()
+        # root.after(100, self.listen_for_results())
+
+    # Pin A event handler
+    def pin_a_rising(self):
+        if self.pin_b.is_pressed:
+            self.moveFilePointer('+')
+            self.showImage()
+
+            # print('-1')
+            # eventq.put(-1) # pin A rising while B is active is a counter-clockwise turn
+
+    # Pin B event handler
+    def pin_b_rising(self):
+        if self.pin_a.is_pressed:
+            self.moveFilePointer('-')
+            self.showImage()
+
+            # print('1')
+            # eventq.put(1) # pin B rising while A is active is a clockwise turn
     
     def detectedRotaryChange(self, event):
+        print("Rotary changed")
         clkState = GPIO.input(clk)
         cntState = GPIO.input(cnt)
         if clkState != self.clkLastState:
             if cntState != clkState:
                 self.ROTARY_COUNTER += 1
+                # self.the_queue.put('plus one')
             else:
                 self.ROTARY_COUNTER -= 1
+                # self.the_queue.put('minus one')
         self.clkLastState = clkState
-        sleep(0.01)
+        # sleep(0.1)
         
         self.buildFilePointer()
-        self.showImage()
+        # self.showImage()
     
     def showImage(self):
         self.img = ImageTk.PhotoImage(Image.open(self.FILE_PATH, 'r'))
@@ -66,8 +102,10 @@ class Slideshow:
             self.ROTARY_COUNTER = 1
         elif self.ROTARY_COUNTER < 1:
             self.ROTARY_COUNTER = self._LIMIT
+        sys.stdout.flush()
         print(self.ROTARY_COUNTER)
-            
+        sys.stdout.flush()
+
         self.FILE_PATH = self._DIRECTORY + str(self.ROTARY_COUNTER) + self._EXTENSION
 
     def moveFilePointer(self, command):
@@ -96,6 +134,16 @@ class Slideshow:
         print("Decrement the count")
         self.moveFilePointer('-')
         self.showImage()
+
+    def listen_for_results(self):
+        print('Listening for results: ')
+        try:
+            message = self.the_queue.get()
+            print(message)
+            # root.after(100, self.listen_for_results())
+        except queue.Empty:
+            root.after(100, self.listen_for_results())
+
     
 # Create the root window
 root = Tk()
