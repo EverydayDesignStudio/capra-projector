61c73af59886cd7a4dd4b0ed91e743f880cd4654
jordanrw <jordan_white@sfu.ca>
2019-04-15 15:22:16 -0700

    Add pitch and roll example

---
+++ sensors/pitch_roll.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import math
+
+from envirophat import motion, analog
+
+unit = 'hPa'  # Pressure unit, can be either hPa (hectopascals) or Pa (pascals)
+
+
+def write(line):
+    sys.stdout.write(line)
+    sys.stdout.flush()
+
+
+write("--- Enviro pHAT Monitoring ---")
+
+try:
+    while True:
+        #mag_values = motion.magnetometer()
+        acc_values = [round(x, 2) for x in motion.accelerometer()]
+        #mx = mag_values[0]
+        #my = mag_values[1]
+        #mz = mag_values[2]
+        ax = acc_values[0]
+        ay = acc_values[1]
+        az = acc_values[2]
+        
+        pitch = 180 * math.atan(ax/math.sqrt(ay*ay + az*az))/math.pi
+        roll = 180 * math.atan(ay/math.sqrt(ax*ax + az*az))/math.pi
+        
+        orientation = ''
+        
+        if roll > -45 and roll < 45:
+            orientation = 'landscape'
+        elif roll >= 45 or roll <= -45:
+            orientation = 'vertical'
+
+        output = """
+Accelerometer: {ax}g {ay}g {az}g
+
+Pitch: {p}
+Roll: {r}
+
+Orientation: {o}
+
+""".format(
+            ax=acc_values[0],
+            ay=acc_values[1],
+            az=acc_values[2],
+            p = pitch,
+            r = roll,
+            o = orientation
+        )
+
+        output = output.replace("\n", "\n\033[K")
+        write(output)
+        lines = len(output.split("\n"))
+        write("\033[{}A".format(lines - 1))
+
+        time.sleep(.1)
+
+except KeyboardInterrupt:
+    pass
+
